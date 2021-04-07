21c1e18b7523fce7efb72833ada438b108276cd0
keer2345 <keer2345@gmail.com>
2021-04-07 22:47:15 +0800

    update an object

--- src/main/java/com/example/amigoscode/controller/StudentController.java
+++ src/main/java/com/example/amigoscode/controller/StudentController.java
@@ -8,8 +8,10 @@ import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -28,7 +30,15 @@ public class StudentController {
   }
 
   @DeleteMapping(path = "{studentId}")
-  public void deleteStudent(@PathVariable("studentId") Long id){
+  public void deleteStudent(@PathVariable("studentId") Long id) {
     studentService.deleteStudent(id);
+  }
+
+  @PutMapping(path = "{studentId}")
+  public void updateStudent(
+      @PathVariable("studentId") Long id,
+      @RequestParam(required = false) String name,
+      @RequestParam(required = true) String email) {
+    studentService.updateStudent(id, name, email);
   }
 }
--- src/main/java/com/example/amigoscode/service/StudentService.java
+++ src/main/java/com/example/amigoscode/service/StudentService.java
@@ -3,7 +3,9 @@ package com.example.amigoscode.service;
 import com.example.amigoscode.entity.Student;
 import com.example.amigoscode.repository.StudentRepo;
 import java.util.List;
+import java.util.Objects;
 import java.util.Optional;
+import javax.transaction.Transactional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -32,5 +34,24 @@ public class StudentService {
       throw new IllegalStateException("student with id [ " + id + " ] does not exists.");
     }
     studentRepo.deleteById(id);
+  }
+
+  @Transactional
+  public void updateStudent(Long id, String name, String email) {
+    Student student =
+        studentRepo
+            .findById(id)
+            .orElseThrow(
+                () -> new IllegalStateException("student with id [ " + id + " ] does not exists!"));
+    if (name != null && name.length() > 0 && !Objects.equals(student.getName(), name)) {
+      student.setName(name);
+    }
+    if (email != null && email.length() > 0 && !Objects.equals(student.getEmail(), email)) {
+      Optional<Student> studentOptional = studentRepo.findStudentByEmail(email);
+      if (studentOptional.isPresent()) {
+        throw new IllegalStateException("email token");
+      }
+      student.setEmail(email);
+    }
   }
 }
